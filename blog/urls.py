6f04ee46bbaacb68443741dc3edb3ec9900727e2
Marcus <marcusmorrow319@gmail.com>
2020-05-22 15:28:34 -0500

    Can now delete comments

--- blog/urls.py
+++ blog/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
     path('drafts/', views.post_draft, name='post_drafts_list'),
     path('pub/<int:pk>', views.post_publish, name='post_publish'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/remove/', views.remove_comment, name='remove_comment'),
 
 ]
